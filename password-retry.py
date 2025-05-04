c139ac86c23593b59efc09717a35f8f1821e2db7
ck <albertckchang@gmail.com>
2025-05-04 11:48:01 +0800

    Ver1_done

---
+++ password-retry.py
@@ -0,0 +1,15 @@
+password = 'a123456'
+count = 1
+
+while True:
+	usertest = input('請輸入密碼:')
+	if usertest != password and count < 3:
+	   print('密碼錯誤! 還有', 3 - count ,'次機會輸入密碼')
+	   #print('還有可嘗試機會:', 3 - count)
+	   count = count +1
+	elif usertest != password and count == 3:
+		print('不能再嘗試輸入密碼了!')
+		break
+	elif usertest == password:
+		print('密碼正確')
+		break
